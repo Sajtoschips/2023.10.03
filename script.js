604dbbaa4e4896e8d673fecb5d23acb6c8a965d0
Sajtoschips <benceboglari@gmail.com>
2023-10-03 09:11:58 +0200

    init

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-let urlap = document.querySelectorAll('.urlap');
-let tablazat = document.querySelectorAll('.tab');
+let urlap = document.querySelector('.urlap');
+let tablazat = document.querySelector('.tab');
+
+function hozzaad(event){
+    event.preventDefault();
+    // console.log('OK')
+    let dolgozo = {}; //üres objektum
+    dolgozo.nev = document.querySelector('#nev').value;
+
+    console.log(dolgozo)
+}
+
+
+urlap.addEventListener('submit',hozzaad)
