75cc472d4b112d8be56866108587bc575846b049
Sajtoschips <benceboglari@gmail.com>
2023-10-03 09:18:17 +0200

    init

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 let urlap = document.querySelector('.urlap');
-let tablazat = document.querySelector('.tab');
+let tablazat = document.querySelector('.tablazat');
+
+let dolgozok = [];
 
 function hozzaad(event){
     event.preventDefault();
     // console.log('OK')
     let dolgozo = {}; //üres objektum
     dolgozo.nev = document.querySelector('#nev').value;
+    dolgozo.email = document.querySelector('#email').value;
+    dolgozo.szuletesiDatum = document.querySelector('#szuletesi-datum').value;
+    dolgozo.neme = document.querySelector('input[name="neme"]:checked').value;
 
     console.log(dolgozo)
+    dolgozok.push(dolgozo); // .push() a végén bővíti a tömböt
 }
 
 
-urlap.addEventListener('submit',hozzaad)
+urlap.addEventListener('sumbit',hozzaad);
